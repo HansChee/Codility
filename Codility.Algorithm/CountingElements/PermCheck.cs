e9d3cff0580e804f8bcd629324a376d6230ee53e
qihang006@gmail.com <qihang006@gmail.com>
2018-05-01 16:32:45 +1000

    perm check not finished

---
+++ Codility.Algorithm/CountingElements/PermCheck.cs
@@ -0,0 +1,19 @@
+﻿/**
+ * https://app.codility.com/programmers/lessons/4-counting_elements/perm_check/
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codility.Algorithm.CountingElements
+{
+    public partial class Solution
+    {
+        public int PermCheck(int[] A)
+        {
+            // to do
+            return A[0];
+        }
+    }
+}
