4c47c05654b9dcacade8155fd6593fb93e929f8f
Hang Qi <qihang001@outlook.com>
2018-05-03 21:39:30 +1000

    missing integer

---
+++ Codility.Algorithm/CountingElements/MissingInteger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codility.Algorithm.CountingElements
+{
+    public partial class Solution
+    {
+        public static int MissingInteger(int[] A)
+        {
+            bool[] B = new bool[A.Length + 1];
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] > 0 && A[i] <= A.Length && !B[A[i]])
+                {
+                    B[A[i]] = true;
+                }
+            }
+
+            for (int j = 1; j < B.Length; j++)
+            {
+                if (!B[j])
+                {
+                    return j;
+                }
+            }
+            return B.Length;
+        }
+    }
+}
---
+++ Codility.Test/CountingElements/TestMissingInteger.cs
@@ -0,0 +1,44 @@
+﻿using Codility.Algorithm.CountingElements;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Codility.Test.CountingElements
+{
+    [TestClass]
+    public class TestMissingInteger
+    {
+        int[] A;
+
+        [TestMethod]
+        public void TestSample()
+        {
+            A = new int[] { 1, 3, 6, 4, 1, 2 };
+
+            var result = Solution.MissingInteger(A);
+
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void TestSample1()
+        {
+            A = new int[] { 1, 2, 3 };
+
+            var result = Solution.MissingInteger(A);
+
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void TestSample2()
+        {
+            A = new int[] { -1, -3 };
+
+            var result = Solution.MissingInteger(A);
+
+            Assert.AreEqual(1, result);
+        }
+    }
+}
